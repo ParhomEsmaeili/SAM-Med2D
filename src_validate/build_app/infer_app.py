f3e78746becc9dce3139f26ae652ec6722d3e400
ParhomEsmaeili <parhom.esmaeili@gmail.com>
2025-04-28 18:16:47 +0100

    Modifying the handling of background bbox prompts, as this is not a valid prompt type for SAMMed2d

--- src_validate/build_app/infer_app.py
+++ src_validate/build_app/infer_app.py
@@ -410,21 +410,22 @@ class InferApp:
             p_dict = (is_state['interaction_torch_format']['interactions'], is_state['interaction_torch_format']['interactions_labels'])
             
             if p_dict[0]['bboxes'] is not None and p_dict[1]['bboxes_labels'] is not None:
-                #We will remove any background bboxes here. SAM-MED2D cannot handle these (nor does it have any meaning in the context within which they use this.)
+                #We will flag any background bboxes here. SAM-MED2D cannot handle these (nor does it have any meaning in the context within which they use this.)
                 if not any([i == 1 for i in p_dict[1]['bboxes_labels']]): 
                     p_dict[0]['bboxes'] = None
                     p_dict[1]['bboxes_labels'] = None 
                 else:
-                    #There is at least one non-background bbox. See if there are any background bboxes, and find their indices. Then delete accordingly.
-                    bbox_list = []
-                    bbox_lb_list = []
-                    for i,j in zip(p_dict[0]['bboxes'], p_dict[1]['bboxes_labels']):
-                        if j == 1:
-                            bbox_list += [i] 
-                            bbox_lb_list += [j] 
-                    p_dict[0]['bboxes'] = bbox_list
-                    p_dict[1]['bboxes_labels'] = bbox_lb_list
-                    assert p_dict[0]['bboxes'] != [] and p_dict[1]['bboxes_labels'] != []
+                    #There is a background bboxes, SAMMed2D doesn't understand what this means and could break the system.
+                    # bbox_list = []
+                    # bbox_lb_list = []
+                    # for i,j in zip(p_dict[0]['bboxes'], p_dict[1]['bboxes_labels']):
+                    #     if j == 1:
+                    #         bbox_list += [i] 
+                    #         bbox_lb_list += [j] 
+                    # p_dict[0]['bboxes'] = bbox_list
+                    # p_dict[1]['bboxes_labels'] = bbox_lb_list
+                    # assert p_dict[0]['bboxes'] != [] and p_dict[1]['bboxes_labels'] != []
+                    raise Exception('Was presented with bboxes delineating background only, SAM-Med2D cannot handle this formulation of prompts.')
             #Determine the prompt type from the input prompt dictionaries: Not sure if intersection is optimal for catching exceptions here.
             provided_ptypes = list(set([k for k,v in p_dict[0].items() if v is not None]) & set([k[:-7] for k,v in p_dict[1].items() if v is not None]))
             
